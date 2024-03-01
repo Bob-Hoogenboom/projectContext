5925c2049646d71e4c86f00e5d3591a6406618c0
cielke <cyascoo@gmail.com>
2024-03-01 22:38:33 +0100

    Added UI's for menus
    - added "MainMenu" & "Controls" scenes
    - added main menu with "start, controls & exit" buttons
    - added GameManager script
    - added scenes to the build settings

---
+++ ProjContext/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CharConTest");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
